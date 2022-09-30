e6e951596788ea6174675a1f5926a36d1ce39cd0
Emre YILMAZ <107490353+emcemen@users.noreply.github.com>
2022-09-30 14:18:51 -0400

    Day6 T1 created TestNg Created

--- src/test/java/com/cydeo/tests/day5_testNG_dropdown/T4_TestNG_Selenium.java
+++ src/test/java/com/cydeo/tests/day5_testNG_dropdown/T4_TestNG_Selenium.java
@@ -1,4 +1,43 @@
 package com.cydeo.tests.day5_testNG_dropdown;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class T4_TestNG_Selenium {
+   WebDriver driver;
+
+        @BeforeMethod
+        public void setUpMethod() {
+        // with this we dont need main method now
+        //Step 1 - Set uo Browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+        @Test
+        public void googleTitle(){
+            //Step 2 - Navigate to the Google Page
+
+            driver.get("https://www.google.com");
+
+        //Step 3- Verify if the title is "Google"
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Google";
+
+        Assert.assertEquals(actualTitle,expectedTitle,"Title is not Matching");
+
+         }
+
+         @AfterMethod
+         public void tearDownMethod(){
+         driver.quit();
+         }
+
 }
---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframe_windows/DropdownPractices.java
@@ -0,0 +1,100 @@
+package com.cydeo.tests.day6_alerts_iframe_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class DropdownPractices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        // 1. Set up browser
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://practice.cybertekschool.com/dropdown");
+
+    }
+
+    @Test
+    public void dropdownTask4() throws InterruptedException {
+
+        Select selectState = new Select(driver.findElement(By.xpath("//select[@id='state']")));
+
+//        3. Select Illinois using visible text
+        selectState.selectByVisibleText("Illinois");
+
+        Thread.sleep(3000);
+
+//        4. Select Virginia using value
+        selectState.selectByValue("VA");
+
+//        5. Select California using index
+        selectState.selectByIndex(5);// it is hard way if there are too many options you need to count all options
+
+//        6. Verify final selected option is California.
+        String actualFinalOption = selectState.getFirstSelectedOption().getText();
+        String expectedFinalOption = "California" ;
+
+        Assert.assertEquals(actualFinalOption,expectedFinalOption,"Final selected option verification is failed!");
+
+
+        //finding index number from options
+        int index=-1;
+        for (WebElement eachState : selectState.getOptions()) {
+            index++;
+            if(eachState.getText().equals("California")){
+                System.out.println(index);
+            }
+        }
+
+    }
+
+    @Test
+    public void dropdownTask7() throws InterruptedException {
+        Select selectLanguage = new Select(driver.findElement(By.xpath("//select[@name='Languages']")));
+
+//        3. Select all the options from multiple select dropdown.
+        //        4. Print out all selected values.
+        for (WebElement eachLanguage : selectLanguage.getOptions()) {
+            eachLanguage.click();
+            System.out.println("eachLanguage.getText() = " + eachLanguage.getText());
+            Thread.sleep(3000);
+        }
+
+        Thread.sleep(3000);
+
+//        5. Deselect all values.
+        selectLanguage.deselectAll();
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
